9b9163ebcc63b5da7595acc80aa030f37964f59e
Ann test <kolesnikova.88@mail.ru>
2024-01-03 16:46:02 +0400

    methods

--- netology_bd_python_2.py
+++ netology_bd_python_2.py
@@ -1,5 +1,82 @@
 import psycopg2
 
+def choose_method(select_request):
+    if int(select_request) == 1:
+        first_name = (input('Введите имя клиента: '))
+        last_name = (input('Введите фамилию клиента: '))
+        email = (input('Введите email клиента: '))
+        add_client(conn, first_name, last_name, email)
+    elif int(select_request) == 2:
+        data_сlient = (input('Введите id клиента: '))
+        phone = (input('Введите телефон клиента: '))
+        add_phone(conn, data_сlient[0], phone)
+        cur.execute("SELECT id, client_id, phone from telefone_сlient")
+        phones = cur.fetchall()
+        print(phones)
+    elif int(select_request) == 3:
+        data_сlient = (input('Введите id клиента: '))
+
+        first_name = (input('Введите имя клиента для изменения: '))
+        last_name = (input('Введите фамилию клиента для изменения: '))
+        email = (input('Введите email клиента для изменения: '))
+        cur.execute("SELECT id, first_name, last_name, email from сlient")
+
+        rows = cur.fetchall()
+        print(rows)
+        for row in rows:
+
+            if first_name and last_name and email:
+                change_client(conn, data_сlient, first_name, last_name, email);
+            elif first_name and last_name:
+                change_client(conn, data_сlient, first_name, last_name, row[3]);
+            elif first_name and email:
+                change_client(conn, data_сlient, first_name, row[2], email);
+            elif last_name:
+                change_client(conn, data_сlient, row[1], last_name, row[3]);
+            elif email:
+                change_client(conn, data_сlient, row[1], row[2], email);
+            elif last_name and email:
+                change_client(conn, data_сlient, row[1], row[2], row[3]);
+            elif first_name:
+                change_client(conn, data_сlient, first_name, row[2], row[3]);
+            else:
+                change_client(conn, data_сlient, row[1], row[2], row[3]);
+
+
+    elif int(select_request) == 3:
+        data_сlient = (input('Введите id клиента: '))
+        data_id_tel = (input('Введите id номера телефона для его удаления: '))
+        cur.execute("SELECT id, client_id, phone from telefone_сlient")
+        rows = cur.fetchall()
+        print(rows)
+        for row in rows:
+            delete_phone(data_id_tel, data_сlient, row[2])
+    elif int(select_request) == 5:
+        data_сlient = (input('Введите id клиента для его удаления: '))
+        data_id_tel = (input('Введите id номера телефона клиента для его удаления: '))
+        cur.execute("SELECT id, client_id, phone from telefone_сlient")
+        phones = cur.fetchall()
+        print(phones)
+        for row in phones:
+            delete_phone(data_id_tel, data_сlient, row[2])
+        cur.execute("SELECT id, first_name, last_name, email from сlient")
+        rows = cur.fetchall()
+        print(rows)
+        delete_client(data_сlient, data_сlient)
+    else:
+
+        first_name = (input('Введите имя клиента для его поиска: '))
+        last_name = (input('Введите фамилию клиента для его поиска: '))
+        email = (input('Введите email клиента для его поиска: '))
+
+        if first_name or last_name or email:
+            cur.execute("SELECT id, first_name, last_name, email from сlient")
+
+            rows = cur.fetchall()
+            for row in rows:
+                if first_name == row[1] or last_name == row[2] or email == row[3]:
+                    print(row)
+
 
 def create_db(conn):
   # создание таблиц
@@ -18,6 +95,7 @@ def create_db(conn):
         """)
 
   print("Table created successfully")
+
 
 def del_table(conn):
     # удаление таблиц
@@ -58,6 +136,7 @@ def change_client(conn, client_id, first_name=None, last_name=None, email=None, 
     res = cur.fetchone()
     return res
 
+
 def delete_phone(conn, client_id, phone):
     cur.execute("""
           DELETE FROM telefone_сlient WHERE id=%s;
@@ -67,87 +146,39 @@ def delete_phone(conn, client_id, phone):
           """)
 
 
-
 def delete_client(conn, client_id):
-  cur.execute("""
+    delete_phone(conn, client_id, phones)
+    cur.execute("""
+              DELETE FROM сlient WHERE id=%s;
+              """, (conn,))
+    cur.execute("""
           SELECT * FROM сlient;
           """)
-  print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
 
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
-  pass
+    cur.execute(
+        'SELECT id, first_name, last_name, email FROM сlient WHERE id=%s;',
+        (first_name, last_name, email)
+    )
+
 
 
 with psycopg2.connect(database="netology_bd", user="postgres", password="netology") as conn:
   rows = []
   with conn.cursor() as cur:
-      select_request = (input('Выберете число, чтобы реализовать следующие действия(добавить нового клиента-1;'
-                              'добавить телефон для существующего клиента-2;изменить данные о клиенте-3;'
-                              ' удалить телефон для существующего клиента-4;удалить существующего клиента-5;'
-                              'найти клиента по его данным: имени, фамилии, email или телефону -6): '))
+      select_request = (input('''Выберете число, чтобы реализовать следующие действия
+                                (добавить нового клиента: 1;
+                                 добавить телефон для существующего клиента: 2;
+                                 изменить данные о клиенте: 3;
+                                 удалить телефон для существующего клиента: 4;
+                                 удалить существующего клиента: 5;
+                                 найти клиента по его данным: имени, фамилии, email или телефону: 6): '''))
 
 
       create_db(conn)
+      choose_method(select_request)
       #del_table(conn)
-
-
-
-      #data_сlient = add_client(conn, first_name, last_name, email)
-      #data_phone = add_phone(conn,data_сlient[0],phone)
-
-      if int(select_request) == 1:
-          first_name = (input('Введите имя клиента: '))
-          last_name = (input('Введите фамилию клиента: '))
-          email = (input('Введите email клиента: '))
-          add_client(conn, first_name, last_name, email)
-      elif int(select_request) == 2:
-          data_сlient = (input('Введите id клиента: '))
-          phone = (input('Введите телефон клиента: '))
-          add_phone(conn, data_сlient[0], phone)
-          cur.execute("SELECT id, client_id, phone from telefone_сlient")
-          phones = cur.fetchall()
-          print(phones)
-      elif int(select_request) == 3:
-          data_сlient = (input('Введите id клиента: '))
-
-          first_name = (input('Введите имя клиента для изменения: '))
-          last_name = (input('Введите фамилию клиента для изменения: '))
-          email = (input('Введите email клиента для изменения: '))
-          cur.execute("SELECT id, first_name, last_name, email from сlient")
-
-          rows = cur.fetchall()
-          print(rows)
-          for row in rows:
-
-              if first_name and last_name and email:
-                  change_client(conn, data_сlient, first_name, last_name, email);
-              elif first_name and last_name:
-                  change_client(conn, data_сlient, first_name, last_name, row[3]);
-              elif first_name and email:
-                  change_client(conn, data_сlient, first_name, row[2],email);
-              elif last_name:
-                  change_client(conn, data_сlient,row[1], last_name, row[3]);
-              elif email:
-                  change_client(conn, data_сlient, row[1], row[2], email);
-              elif last_name and email:
-                  change_client(conn, data_сlient,row[1],  row[2],  row[3]);
-              elif first_name:
-                  change_client(conn, data_сlient, first_name,  row[2],  row[3]);
-              else:
-                  change_client(conn, data_сlient, row[1],  row[2],  row[3]);
-      else:
-          data_сlient = (input('Введите id клиента: '))
-          data_id_tel = (input('Введите id номера: '))
-          cur.execute("SELECT id, client_id, phone from telefone_сlient")
-          rows = cur.fetchall()
-          print(rows)
-          for row in rows:
-             delete_phone(data_id_tel, data_сlient, row[2])
-
-
-
-
 
       cur.execute("SELECT id, first_name, last_name, email from сlient")
       rows = cur.fetchall()
@@ -158,13 +189,6 @@ with psycopg2.connect(database="netology_bd", user="postgres", password="netolog
 
       print(rows)
       print(phones)
-      '''for row in rows:
-            print("id =", row[0])
-            print("first_name =", row[1])
-            print("last_name =", row[2])
-            print("email =", row[3])'''
-
-
 
 print("Operation done successfully")
 
